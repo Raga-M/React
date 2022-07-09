055977f6d691241525725b1c83110af15691f159
Raga-M <ragavendran200@gmail.com>
2022-07-09 12:01:37 +0530

    1stCommit

---
+++ quiz/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="quiz">
+<div>
+  <h1> question</h1>
+</div>
+<div>
+  <ul>
+    <li>option A</li>
+    <li>option B</li>
+    <li>option C</li>
+    <li>option D</li>
+  </ul>
+</div>
+    </div>
+  );
+}
+
+export default App;
